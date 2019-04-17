aebafd28a919db8fc1ad9cdf0b1de3bc84b116d6
Jonathan Payne <paynejd@gmail.com>
2019-04-17 12:25:37 -0400

    Updated documentation for importinit.py

--- init/importinit.py
+++ init/importinit.py
@@ -1,23 +1,24 @@
 """
 Use to import content into OCL to initialize the PEPFAR DATIM environment.
+Before running, edit import_filenames variable with the list of files you wish to import.
 
 1. Start by creating the org/source structure by importing one of these:
 1a. If synchronizing with all content (MER, SIMS, Mechanisms, Tiered Site Support, & DATIM-MOH):
   datim_init_all.json -- Org:PEPFAR; Sources:MER,DATIM-MOH,SIMS,Mechanisms,Tiered-Site-Support; DATIM-MOH collection;
     initial repo versions
-1b. If only doing DATIM-MOH:
+1b. If only DATIM-MOH, then import:
   datim_init_moh_only.json -- Org:PEPFAR; Sources:DATIM-MOH; DATIM-MOH collection; initial empty repo version
 
 2. OCL collections that stay in sync with a DHIS2 Dataset must be pre-defined in OCL. Import the following if
 synchronizing with MER, SIMS, or Tiered Site Support (not needed for DATIM-MOH or Mechanisms):
-  dhis2datasets.json -- Collections and initial empty versions for MER, SIMS, and Tiered Site Support
+  dhis2datasets.json -- Collections and their initial empty versions for MER, SIMS, and Tiered Site Support
 
 3. Tiered site support content is static and can be imported using the provided JSON file. If using Tiered Site
 Support, then import the following:
   tiered_support.json -- Concepts and Mappings for Tiered Site Support
 
-TODO:
-- Create empty source version for Tiered Site Support? Add references for Tiered Site Support collections?
+Notes:
+- No repo versions and no collection references are created for Tiered Site Support
 """
 from ocldev.oclfleximporter import OclFlexImporter
 import settings
@@ -32,17 +33,14 @@ import_filenames_all = {
 import_filenames_datim_moh_only = {
     'json_datim_moh': 'datim_init_only_moh.json',
 }
-#import_filenames = import_filenames_datim_moh_only
-import_filenames = {'test':'test.json'}
+import_filenames = import_filenames_datim_moh_only
 
 # OCL Settings - JetStream Staging user=datim-admin
-#ocl_api_url_root = settings.ocl_api_url_production
-#ocl_api_token = settings.api_token_production_datim_admin
 ocl_api_url_root = settings.ocl_api_url_staging
-ocl_api_token = settings.api_token_staging_paynejd
+ocl_api_token = settings.api_token_staging_datim_admin
 
 # Recommend running with test mode set to True before running for real
-test_mode = True
+test_mode = False
 limit = 0
 
 for k in import_filenames:

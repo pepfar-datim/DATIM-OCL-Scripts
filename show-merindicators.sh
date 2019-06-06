04f2ace86d916c77c7552eba3be7004244844ee4
maurya <maurya@openmrs.org>
2019-06-06 13:28:24 -0400

    Adding datim folder to the shell scripts

--- show-merindicators.sh
+++ show-merindicators.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python /opt/ocl_datim/datimshowmer.py $1 $2
+python /opt/ocl_datim/datim/datimshowmer.py $1 $2
--- show-sims.sh
+++ show-sims.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python /opt/ocl_datim/datimshowsims.py $1 $2
+python /opt/ocl_datim/datim/datimshowsims.py $1 $2

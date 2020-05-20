995f381106b9db271efc3d32395c3874231d0c29
maurya <srikumma@regenstrief.org>
2020-05-20 10:50:02 -0400

    Create oclPassThroughRequests.py

---
+++ oclPassThroughRequests.py
@@ -0,0 +1,101 @@
+"""
+Script to retrieve QMAP status from OCL and return as JSON.
+Designed to work with PEPFAR IMAP and QMAP resources.
+
+python bulkImportStatus.py --env=staging -t="my-api-token-here"
+    --bulkImportId=d6ac3dd2-e565-455c-b29a-f7d859fd1fe8-datim-admin
+"""
+import requests
+import json
+import argparse
+
+# Script constants
+APP_VERSION = '0.1.0'
+OCL_ENVIRONMENTS = {
+    'qa': 'https://api.qa.openconceptlab.org',
+    'staging': 'https://api.staging.openconceptlab.org',
+    'production': 'https://api.openconceptlab.org',
+    'demo': 'https://api.demo.openconceptlab.org',
+}
+
+
+# Argument parser validation functions
+def ocl_environment(string):
+    if string not in OCL_ENVIRONMENTS:
+        raise argparse.ArgumentTypeError(
+            'Argument "env" must be %s' % ', '.join(OCL_ENVIRONMENTS.keys()))
+    return OCL_ENVIRONMENTS[string]
+
+
+# Checks OCL bulk import status
+def check_bulk_import_status(bulkImportId='', ocl_env_url='', ocl_api_token='',
+                             import_result_format=''):
+    import_result_formats = ['report', 'json', 'summary']
+    if import_result_format not in import_result_formats:
+        import_result_format = 'summary'
+
+    ocl_api_headers = {'Content-Type': 'application/json'}
+    if ocl_api_token:
+        ocl_api_headers['Authorization'] = 'Token ' + ocl_api_token
+    import_status_url = "%s/manage/bulkimport/?task=%s&result=%s" % (
+        ocl_env_url, bulkImportId, import_result_format)
+    response = requests.get(import_status_url, headers=ocl_api_headers)
+    response.raise_for_status()
+    return response.text
+
+# get QMAP domain details
+def getQMAPDomainDetails(ocl_env_url='', domain=''):
+    ocl_api_headers = {'Content-Type': 'application/json'}
+    qmapDetailsURL = "%s/orgs/%s" % (
+        ocl_env_url, domain)
+    response = requests.get(qmapDetailsURL, headers=ocl_api_headers)
+    response.raise_for_status()
+    return response.text
+
+
+# Configure
+parser = argparse.ArgumentParser("bulkImportStatus", description="Get Bulk Import Status from OCL")
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument('--env', help='Name of the OCL API environment', type=ocl_environment)
+group.add_argument('--envurl', help='URL of the OCL API environment')
+parser.add_argument('--bulkImportId', help='Bulk Import Status ID', required=False)
+parser.add_argument('-t', '--token', help='OCL API token', required=True)
+parser.add_argument(
+    '-v', '--verbosity', help='Verbosity level: 0 (default), 1, or 2', default=0, type=int)
+parser.add_argument('--version', action='version', version='%(prog)s v' + APP_VERSION)
+parser.add_argument(
+    '--format', help='Format of bulk import results to return from OCL', default="summary")
+parser.add_argument(
+    '--domain', help='Domain of bulk import results to return from OCL', required=False)
+parser.add_argument(
+    '--requestType', help='Type of Passthrough Request', required=True)
+args = parser.parse_args()
+ocl_env_url = args.env if args.env else args.env_url
+
+# Display debug output
+if args.verbosity > 1:
+    print args
+
+response = ''
+try:
+    if (args.requestType=="bulkImportStatus"):
+        response = check_bulk_import_status(
+            bulkImportId=args.bulkImportId, ocl_env_url=ocl_env_url,
+            ocl_api_token=args.token, import_result_format=args.format)
+    if (args.requestType=="qmapDetails"):
+        response = getQMAPDomainDetails(ocl_env_url=ocl_env_url, domain=args.domain)
+except Exception as e:
+    output_json = {
+        "status": "Error",
+        "status_code": 0,
+        "message": response
+    }
+else:
+    output_json = {
+        "status": "",
+        "status_code": 0,
+        "message": response
+    }
+
+if output_json:
+    print json.dumps(output_json)

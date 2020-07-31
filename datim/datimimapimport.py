15d8905a729e7b7d26230bee963f03d6020648ec
Jonathan Payne <paynejd@gmail.com>
2020-07-31 09:46:55 -0400

    Fixed bulk import queue key

--- datim/datimimapimport.py
+++ datim/datimimapimport.py
@@ -125,10 +125,9 @@ class DatimImapImport(datimbase.DatimBase):
         if import_list and not self.test_mode:
             self.vlog(1, 'Bulk importing %s resources to OCL...' % len(import_list))
             # TODO: Implement better OclBulkImporter response -- a new class OclBulkImportResponse?
-            bulk_import_queue_key = '%s%s' % (imap_input.country_code, imap_input.period)
             bulk_import_response = ocldev.oclfleximporter.OclBulkImporter.post(
                 input_list=import_list, api_token=self.oclapitoken, api_url_root=self.oclenv,
-                queue=bulk_import_queue_key)
+                queue=imap_input.country_org)
             bulk_import_response.raise_for_status()
             task_id = bulk_import_response.json()['task']
             self.vlog(1, 'BULK IMPORT TASK ID: %s' % task_id)

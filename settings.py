21cb4ce814e086bcbe24fc77292c93dd7f69fd27
Jonathan Payne <paynejd@gmail.com>
2022-02-10 12:20:12 -0500

    Minor updates for OCL's new dev env & expansions model

--- settings.py
+++ settings.py
@@ -16,17 +16,16 @@ api_token_staging_root = ''
 api_token_staging_datim_admin = ''
 api_token_production_root = ''
 api_token_production_datim_admin = ''
+api_token_dev_root = ''
+api_token_dev_datim_admin = ''
 
 # OCL API URL root - no slash at the end
 ocl_api_url_production = 'https://api.openconceptlab.org'
-ocl_api_url_production_v1 = 'https://api.v1.openconceptlab.org'
-ocl_api_url_production_aws = 'https://api.aws.openconceptlab.org'
 ocl_api_url_staging = 'https://api.staging.openconceptlab.org'
-ocl_api_url_staging_v1 = 'https://api.staging.v1.openconceptlab.org'
-ocl_api_url_staging_aws = 'https://api.staging.aws.openconceptlab.org'
 ocl_api_url_qa = 'https://api.qa.openconceptlab.org'
-ocl_api_url_qa_v1 = 'https://api.qa.v1.openconceptlab.org'
-ocl_api_url_qa_aws = 'https://api.qa.aws.openconceptlab.org'
+ocl_api_url_who_staging = 'https://api.staging.who.openconceptlab.org'
+ocl_api_url_demo = 'https://api.demo.openconceptlab.org'
+ocl_api_url_dev = 'https://api.dev.openconceptlab.org'
 
 # IMAP Mediator URL roots - no slash at the end
 imap_mediator_url_test = 'https://test.ohie.datim.org:5000'

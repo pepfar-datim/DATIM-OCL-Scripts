049dc6f34523267e8feed286b0633b787bfeabd6
Jonathan Payne <paynejd@gmail.com>
2022-08-16 10:37:49 -0400

    Updated FY22 category code process
    Option to source the indicator_category_code directly from the DHIS2 codelist CSV, if defined

--- init/dhis2_moh_csv_to_ocl_json.py
+++ init/dhis2_moh_csv_to_ocl_json.py
@@ -1,18 +1,26 @@
 """
 Converts a DSHI2 formatted CSV codelist to an OCL-formatted JSON bulk import script.
-This has been tested and used for DAA-FY21 and CS-FY21. This would likely work for other
-periods (eg FY20) as well with minimal modification.
+This script was developed for DAA-FY21 and CS-FY21, and also used for DAA-FY22.
+This would likely work for earlier periods (eg FY20) as well with minimal modification.
+
+DAA-FY22 introduced a new column for "indicator_category_code" in the DHIS2 codelist CSV.
+If this field is defined, it is automatically used to define the indicator_category_code
+value for each data element. If it is not available, then an optional mapping file may be
+used (see step 2 below for more details). Otherwise, the indicator_category_code is extracted
+from the first 2 segments of a data element code. Therefore, for the data element
+"HTS_TST_N_MOH_Age_Agg_Sex_Result", the indicator code is set to "HTS_TST".
 
 1. Save DHIS2 codelist to CSV file (eg dhis2_moh_FY21_DAA.csv), eg:
     CS-FY21: https://test.geoalign.datim.org/api/sqlViews/ioG5uxOYnZe/data.html+css?var=dataSets:u069j8gVYTA
     DAA-FY21: https://test.geoalign.datim.org/api/sqlViews/ioG5uxOYnZe/data.html+css?var=dataSets:U7qYX49krHK
     DAA-FY22: https://test.geoalign.datim.org/api/sqlViews/ioG5uxOYnZe/data.html+css?var=dataSets:RGDmmG5taRt
-2. (Optional) Create/update indicator mapping file to link data elements to standardized indicator codes
-   For an example, see `cs_fy21_indicator_maps.csv`. By default (i.e. if there is no mapping defined for a
-   data element), the indicator code is extracted from the first 2 segments of a data element code. Therefore,
-   for the data element "HTS_TST_N_MOH_Age_Agg_Sex_Result", the indicator code is set to "HTS_TST". Use the
-   mapping file if the default behavior is incorrect for a data element.
-3. Update Script Settings for the codelist (modality, fy_only, period, csv_filename, etc.)
+2. (Optional) Create/update indicator mapping file to link data elements to standardized
+   indicator codes. For an example, see `cs_fy21_indicator_maps.csv`. By default, the
+   indicator code is extracted from the first 2 segments of a data element code.
+   Therefore, for the data element "HTS_TST_N_MOH_Age_Agg_Sex_Result", the indicator
+   code is set to "HTS_TST". Use the mapping file if the default behavior is incorrect
+   for a data element.
+3. Update Script Settings for the codelist (MODALITY, FY_ONLY, PERIOD, CSV_FILENAME, etc.)
 4. Run the script and save the outputted OCL bulk import script to a json file:
     python dhis2_moh_csv_to_ocl_json.py > datim_moh_fy21_daa.json
 5. To load the codelist into an OCL environment, update and run importinit.py:
@@ -24,28 +32,28 @@ import ocldev.oclresourcelist
 
 
 # Script settings
-modality = 'DAA'  # e.g. CS or DAA
-fy_only = 'FY22Q1'  # e.g. FY21 or FY22Q1
-period = "DAA-FY22"  # e.g. DAA-FY22Q1, CS-FY21
-csv_filename = 'dhis2_moh_FY22_DAA.csv'  # e.g. dhis2_moh_FY22_DAA.csv
+MODALITY = 'DAA'  # e.g. CS or DAA
+FY_ONLY = 'FY22Q1'  # e.g. FY21 or FY22Q1
+PERIOD = "DAA-FY22Q1"  # e.g. DAA-FY21, DAA-FY22Q1, CS-FY21
+CSV_FILENAME = 'dhis2_moh_FY22_DAA_v2.csv'  # e.g. dhis2_moh_FY22_DAA.csv
 FILENAME_MAP_DE_CODE_TO_INDICATOR_CATEGORY = ''  # e.g. cs_fy21_indicator_maps.csv
-owner = 'PEPFAR'  # e.g. PEPFAR
-owner_type = 'Organization'  # e.g. Organization or User
-debug = False
+OWNER = 'PEPFAR'  # e.g. PEPFAR
+OWNER_TYPE = 'Organization'  # e.g. Organization or User
+DEBUG = False
 
 
 # Source, null disag, and source version configuration
 datim_moh_source = {
     "type": "Source",
-    "id": "DATIM-MOH-%s" % period,
-    "short_code": "DATIM-MOH-%s" % period,
-    "name": "DATIM-MOH-%s" % period,
-    "full_name": "DATIM MOH %s %s Country Alignment Code List" % (modality, fy_only),
-    "owner_type": owner_type,
-    "owner": owner,
+    "id": "DATIM-MOH-%s" % PERIOD,
+    "short_code": "DATIM-MOH-%s" % PERIOD,
+    "name": "DATIM-MOH-%s" % PERIOD,
+    "full_name": "DATIM MOH %s %s Country Alignment Code List" % (MODALITY, FY_ONLY),
+    "owner_type": OWNER_TYPE,
+    "owner": OWNER,
     "description": "",
     "extras": {
-        "Period": period,
+        "Period": PERIOD,
         "datim_moh_codelist": True
     },
     "default_locale": "en",
@@ -70,7 +78,7 @@ null_disag_concept = {
 }
 source_version = {
     "type": "Source Version",
-    "id": "%s.v0" % period,
+    "id": "%s.v0" % PERIOD,
     "description": "Initial release",
     "released": True,
     "source": datim_moh_source['id'],
@@ -80,6 +88,7 @@ source_version = {
 
 
 def find_nth(haystack, needle, n):
+    ''' Return index of nth occurence of a search string '''
     start = haystack.find(needle)
     while start >= 0 and n > 1:
         start = haystack.find(needle, start+len(needle))
@@ -88,6 +97,7 @@ def find_nth(haystack, needle, n):
 
 
 def row_to_coc_concept(row, datim_moh_source):
+    ''' Generate category option combo (COC) concept from a codelist row '''
     concept = {
         "type": "Concept",
         "id": row['categoryoptioncombouid'],
@@ -113,7 +123,12 @@ def row_to_coc_concept(row, datim_moh_source):
 
 
 def row_to_de_concept(row, datim_moh_source, map_de_code_to_indicator_category, debug=False):
-    if row['code'] in map_de_code_to_indicator_category:
+    ''' Generate data element (DE) concept from row in the codelist '''
+
+    # Determine the indicator_category_code for the concept
+    if 'indicator_category_code' in row and row['indicator_category_code']:
+        indicator_category_code = row['indicator_category_code']
+    elif row['code'] in map_de_code_to_indicator_category:
         indicator_category_code = map_de_code_to_indicator_category[row['code']]
         if debug:
             print(row['code'], indicator_category_code)
@@ -121,6 +136,8 @@ def row_to_de_concept(row, datim_moh_source, map_de_code_to_indicator_category, 
         indicator_category_code = row['code'][:find_nth(row['code'], '_', 2)]
         if debug:
             print('***', row['code'], indicator_category_code)
+
+    # Build the OCL-formatted concept
     concept = {
         "type": "Concept",
         "id": row['code'],
@@ -161,6 +178,7 @@ def row_to_de_concept(row, datim_moh_source, map_de_code_to_indicator_category, 
 
 
 def row_to_mapping(row, datim_moh_source):
+    ''' Generate a COC to DE mapping from a codelist row '''
     from_concept_url = '/orgs/%s/sources/%s/concepts/%s/' % (
         datim_moh_source['owner'], datim_moh_source['id'], row['code'])
     to_concept_url = '/orgs/%s/sources/%s/concepts/%s/' % (
@@ -178,12 +196,15 @@ def row_to_mapping(row, datim_moh_source):
     return mapping
 
 
-# Load mapping between indicator categories and data element codes
+# Load mapping between indicator categories and data element codes, if present
+# NOTE: This is not used if an "indicator_category_code" column is present in the codelist
 de_concepts = {}
 coc_concepts = {}
 de_coc_mappings = []
 map_de_code_to_indicator_category = {}
 if FILENAME_MAP_DE_CODE_TO_INDICATOR_CATEGORY:
+    if DEBUG:
+        print('LOADING INDICATOR MAPPING FILE: %s' % FILENAME_MAP_DE_CODE_TO_INDICATOR_CATEGORY)
     with open(FILENAME_MAP_DE_CODE_TO_INDICATOR_CATEGORY) as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
@@ -193,10 +214,10 @@ if FILENAME_MAP_DE_CODE_TO_INDICATOR_CATEGORY:
 de_codes = {}
 missing = []
 found = []
-if debug:
+if DEBUG:
     print('LIST OF UNIQUE DATA ELEMENT CODES MAPPED TO INDICATOR CODES:')
     print('de_name,indicator_code')
-with open(csv_filename) as csvfile:
+with open(CSV_FILENAME) as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
         de_code = row['code']
@@ -205,7 +226,7 @@ with open(csv_filename) as csvfile:
             found.append(de_code)
         if row["code"] not in de_concepts:
             de_concepts[row["code"]] = row_to_de_concept(
-                row, datim_moh_source, map_de_code_to_indicator_category, debug)
+                row, datim_moh_source, map_de_code_to_indicator_category, DEBUG)
         if row["categoryoptioncombouid"] not in coc_concepts:
             coc_concepts[row["categoryoptioncombouid"]] = row_to_coc_concept(row, datim_moh_source)
         de_coc_mappings.append(row_to_mapping(row, datim_moh_source))
@@ -221,7 +242,7 @@ for coc_concept_id in coc_concepts:
 moh_resources += de_coc_mappings
 moh_resources += source_version
 
-if debug:
+if DEBUG:
     print('\n\nCODELIST AS OCL-FORMATTED JSON:')
 
 # Output
